0ba875c42531b537cd8b3f4b2ac81ccd4462cd61
DRK-Jingles <drk_jingles@windowslive.com>
2020-05-26 12:20:09 +1200

    added favourite mode of transport

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 	System.out.println("Hi, my name is Adam");
 	System.out.println("My favourite coulour is black");
+	System.out.println("My favourite mode of transportation is by car");
 	}
 
 }
